2720e9285b685bcc937f4cd50c9b7c79cd3c7d4b
TPM079 <t.mead179@gmail.com>
2026-05-15 01:05:17 +1000

    Update footer copyright and legal config

--- src/config/siteLegal.ts
+++ src/config/siteLegal.ts
@@ -1,14 +1,14 @@
 export const siteLegal = {
   brandName: "PressureCal",
-  siteUrl: "https://pressurecal.com", // TODO: confirm final canonical domain
+  siteUrl: "https://www.pressurecal.com",
   contactEmail: "hello@pressurecal.com",
   businessName: "PressureCal",
-  tradingName: "PressureCal", // TODO: update if different
+  tradingName: "PressureCal",
   abn: "97 288 695 115",
-  billingProvider: "Stripe", // TODO: update if different
-  jurisdiction: "New South Wales, Australia", // TODO: update if needed
+  billingProvider: "Stripe",
+  jurisdiction: "New South Wales, Australia",
   effectiveDate: "3 April 2026",
-  lastUpdated: "3 April 2026",
+  lastUpdated: "15 May 2026",
   companyLocation: "Australia",
   supportResponseNote:
     "We aim to respond to genuine support and privacy enquiries within a reasonable time.",
